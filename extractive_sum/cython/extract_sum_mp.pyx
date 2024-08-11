3fe5173d88bf7f3e5ed0bf8c4647a94e74865c6a
Jack Nguyen <nguyenminhtam7124@gmail.com>
2024-08-11 00:15:21 -0400

    Optimize Cython Code

--- extractive_sum/cython/extract_sum_mp.pyx
+++ extractive_sum/cython/extract_sum_mp.pyx
@@ -1,18 +1,14 @@
-# Save this file as extract_sum_mp.pyx
-
-# Disable deprecated NumPy API
-# define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 import nltk
 import networkx as nx
-import numpy as np
 import multiprocessing as mp
+import numpy as np  # Import numpy for dot product
 
 cimport numpy as cnp
+from libc.stdlib cimport malloc, free
 
 nltk.download('punkt', quiet=True)
 nltk.download('stopwords', quiet=True)
@@ -29,14 +25,14 @@ cpdef cnp.ndarray build_similarity_matrix(list sentences):
     if tfidf_matrix.shape[1] == 0:
         raise ValueError("TF-IDF matrix has zero columns. Check the input data.")
 
-    n_components = min(100, tfidf_matrix.shape[1])
+    cdef int n_components = min(100, tfidf_matrix.shape[1])
     svd = TruncatedSVD(n_components=n_components, random_state=42, n_iter=7)
     reduced_tfidf_matrix = svd.fit_transform(tfidf_matrix)
 
     if np.isnan(reduced_tfidf_matrix).any():
         raise ValueError("NaN values encountered in reduced TF-IDF matrix.")
 
-    similarity_matrix = np.dot(reduced_tfidf_matrix, reduced_tfidf_matrix.T)
+    cdef cnp.ndarray[cnp.float64_t, ndim=2] similarity_matrix = np.dot(reduced_tfidf_matrix, reduced_tfidf_matrix.T)
     return similarity_matrix
 
 cpdef dict rank_sentences(cnp.ndarray similarity_matrix):
@@ -45,17 +41,20 @@ cpdef dict rank_sentences(cnp.ndarray similarity_matrix):
     return scores
 
 cpdef str extract_summary(list sentences, dict scores, int num_sentences=3):
+    cdef list ranked_sentences
+    cdef int i
+    cdef str s
     ranked_sentences = sorted(
         [(scores[i], s) for i, s in enumerate(sentences)], reverse=True)
-    summary = ' '.join(
+    cdef str summary = ' '.join(
         [sentence for _, sentence in ranked_sentences[:num_sentences]])
     return summary
 
 cpdef str process_text(str text):
-    sentences = preprocess_text(text)
-    similarity_matrix = build_similarity_matrix(sentences)
-    scores = rank_sentences(similarity_matrix)
-    summary = extract_summary(sentences, scores)
+    cdef list sentences = preprocess_text(text)
+    cdef cnp.ndarray similarity_matrix = build_similarity_matrix(sentences)
+    cdef dict scores = rank_sentences(similarity_matrix)
+    cdef str summary = extract_summary(sentences, scores)
     return summary
 
 cpdef list mass_extract_summaries(list inputs):
--- extractive_sum/cython/setup.py
+++ extractive_sum/cython/setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup
 from Cython.Build import cythonize
-import numpy
 
 '''
 Compile Cython code with: python3 setup.py build_ext --inplace
 '''
 
 setup(
-    ext_modules=cythonize("extract_sum_mp.pyx"),
-    include_dirs=[numpy.get_include()]
+    ext_modules=cythonize("extract_sum_mp.pyx")
 )
